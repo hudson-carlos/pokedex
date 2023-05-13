7cffb69d46d4e863f08723ac9cc008736532e907
hudson-carlos <hc88806@gmail.com>
2023-05-13 01:28:42 -0400

    feat: add escroll infinit

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -2,15 +2,23 @@ import { useEffect, useState, useRef } from "react";
 import Card from "../Componets/card";
 
 export default () => {
-  const [pageStarting, setPageStarting] = useState<number>(0)  
+  const [page, setPage] = useState<number>(0);  
   const [listPokemons, setListPokemons] = useState<any[]>([]);
   const loadMoreRef = useRef(null);
 
-
   useEffect (() => {
+    getPokeAPI(page);
+  }, [page]);
 
-    getPokeAPI(pageStarting);
-    setPageStarting(page => page + 20);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entities) => {
+      const target = entities[0];
+      
+      if (target.isIntersecting && listPokemons.length > 0) setPage((old: number) => old + 20);
+      
+    });
+    if (loadMoreRef.current) observer.observe(loadMoreRef.current);
+    return () => observer.disconnect();
 
   }, []);
 
@@ -18,10 +26,19 @@ export default () => {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${page}`);
     const {results} = await res.json();
     
-    setListPokemons((list) => [...list, ...results]);
-
+    setListPokemons(old =>  [...old, ...results]);
   }
-    
+
+  function loadingNewList() {
+    return (<p
+      onClick={() => {
+        
+        console.log(listPokemons.length);
+        
+      }} 
+      ref={loadMoreRef}>Carregando mais pokemons...</p>)
+    }
+
   return (
     <div>
       {
@@ -29,13 +46,7 @@ export default () => {
         const urlIMG: string = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index+1}.png`;
         return <Card imgPokemon={urlIMG} namePokemon={name} key={`${name}_${index}`}/>
       })}
-      <p
-      onClick={async () => {
-        await getPokeAPI(pageStarting);
-        setPageStarting(page => page + 20);
-      }}
-      
-      >Carregando mais pokemons...</p>   
+      {loadingNewList()}   
     </div>
   )
 }
